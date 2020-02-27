f2d88cce35f95cf341b478a51440e40628932eb1
shtaz87 <gladilin.aleks@gmail.com>
2020-02-27 23:59:41 +0300

    Create popup.js

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+var modal = document.querySelector(".feedback");
+var btn = document.querySelector(".pop-up-btn");
+var close = document.querySelector(".feedback-close");
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+close.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
